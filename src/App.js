450b308bb57eab4eab601b86f24769acf7259b88
Diganta Nayak <diganta.19032001@gmail.com>
2023-02-17 12:03:32 +0530

    added the error page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SingleProduct from "./SingleProduct"
 import {GlobalStyle } from "./GlobalStyle"
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header.js";
-import Footer from "./components/Footer.js"
+import Footer from "./components/Footer.js";
 
 const App = () => {
 
@@ -49,6 +49,7 @@ const App = () => {
           <Route path="/cart" element={<Cart/>}/>
           <Route path="*" element={<ErrorPage/>}/>
           <Route path="/singleproduct/:id" element={<SingleProduct/>}/>
+          <Route path="*" element={<ErrorPage />}/>
         </Routes>
         <Footer />
     </BrowserRouter>
