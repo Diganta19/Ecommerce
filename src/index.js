b1c6d99e8de10a1e07df12a8a96cb0d5b553a1c9
Diganta Nayak <diganta.19032001@gmail.com>
2023-03-04 19:42:51 +0530

    created a coxtext for fetching the data for product page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "./context/productcontext";
+import { FilterContextProvider } from "./context/filter_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppProvider><App /></AppProvider>);
+root.render(<AppProvider><FilterContextProvider><App /></FilterContextProvider></AppProvider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
