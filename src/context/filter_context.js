e2524386e4ca5b82b6dbd4f02440046387eb77a5
Diganta Nayak <diganta.19032001@gmail.com>
2023-03-12 20:35:42 +0530

    searchbar functionality added in product page

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -10,6 +10,9 @@ const initialState = {
     all_products: [],
     grid_view:true,
     sorting_value : "lowest",
+    filters:{
+        text:"",
+    },
 }
 
 export const FilterContextProvider = ({children}) =>{
@@ -28,14 +31,22 @@ export const FilterContextProvider = ({children}) =>{
         dispatch({type:"GET_SORT_VALUE",payload:userValue});
     }
 
+    const updateFilterValue =(event) =>{
+        let name = event.target.name;
+        let value = event.target.value;
+
+        return dispatch({type:"UPDATE_FILTERS_VALUE", payload: {name,value}})
+    }
+
     useEffect(()=>{
+        dispatch({type:"FILTER_PRODUCTS"})
         dispatch({type:"SORTING_PRODUCTS"})
-    },[state.sorting_value])
+    },[state.sorting_value,state.filters])
 
     useEffect(()=>{
         dispatch({type: "LOAD_FILTER_PRODUCTS",payload:products})
     },[products]);
-    return (<FilterContext.Provider value={{...state,setGridView,setListView,sorting}}>{children}</FilterContext.Provider>)
+    return (<FilterContext.Provider value={{...state,setGridView,setListView,sorting,updateFilterValue}}>{children}</FilterContext.Provider>)
 }
 
 export const useFilterContext = ()=>{
